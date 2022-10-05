4f569cf15f01902d4a1ddf2a59dd6ecac67b480a
hrhabib1 <sayefhabib123@gmail.com>
2022-10-05 10:14:50 +0600

    add to card select

---
+++ js/select.js
@@ -0,0 +1,31 @@
+   let num = 1;
+  document.getElementById('select-1').addEventListener('click', function(){
+        const productSelect = document.getElementById('product-select');
+        productSelect.innerText = +num ;
+        num = num + 1;             
+    })
+  document.getElementById('select-2').addEventListener('click', function(){
+        const productSelect = document.getElementById('product-select');
+        productSelect.innerText = +num ;
+        num = num + 1;             
+    })
+  document.getElementById('select-3').addEventListener('click', function(){
+        const productSelect = document.getElementById('product-select');
+        productSelect.innerText = +num ;
+        num = num + 1;             
+    })
+  document.getElementById('select-4').addEventListener('click', function(){
+        const productSelect = document.getElementById('product-select');
+        productSelect.innerText = +num ;
+        num = num + 1;             
+    })
+  document.getElementById('select-5').addEventListener('click', function(){
+        const productSelect = document.getElementById('product-select');
+        productSelect.innerText = +num ;
+        num = num + 1;             
+    })
+  document.getElementById('select-6').addEventListener('click', function(){
+        const productSelect = document.getElementById('product-select');
+        productSelect.innerText = +num ;
+        num = num + 1;             
+    })
